5588a4db223d43e60cb4cccda7cfce74366a5104
mynamenn <nguleychun@gmail.com>
2020-07-16 10:57:50 +0800

    Added check mandatory fields

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -4,16 +4,17 @@ import Renderer from './Renderer';
 import '../style/style.css';
 
 var link = "";
+var formType = "";
 class MainPage extends React.Component {
     state = {
         newForm: {},
         tasks: {},
         taskIds: [],
         Regex: {},
-        bankList: []
+        bankList: [],
     };
 
-    handleGetData = () => {
+    handleGetData = (form) => {
         axios
             .get('/DemoApp/add')
             .then(response => {
@@ -29,102 +30,186 @@ class MainPage extends React.Component {
             .catch(error => {
                 console.log(error)
             })
-    }
 
-    handleSubmit = (e) => {
-        e.preventDefault();
-        var validate = true;
-        // Check each field
-        this.state.taskIds.map(taskId => {
-            if (this.state.tasks[taskId]['inputField'] === 'TextField' || this.state.tasks[taskId]['inputField'] === 'BankList') {
-                if (document.getElementById(this.state.tasks[taskId]['content']).value !== "") {
-                    if (document.getElementById('alert' + this.state.tasks[taskId]['content']).innerHTML !== "") {
-                        document.getElementById("submit").disabled = false;
-                        validate = false;
-                    }
-                } else {
-                    document.getElementById("submit").disabled = false;
-                    document.getElementById('alert' + this.state.tasks[taskId]['content']).innerHTML = 'Invalid ' + this.state.tasks[taskId]['content'] + '.';
-                    validate = false;
-                }
-            }
-        })
-        // Generate URL
-        if (validate === true) {
-            var url = "https://demo.curlec.com/new-instant-pay?";
-            var description = "Instant%20Pay%20Testing";
-            var amount = document.getElementById('Amount').value;
-            var merchantId = "1";
-            var employeeId = "2";
-            var bankCode = document.getElementById('Bank List').value;
-            var method = "03";
-            link = url + "description=" + description + '&' + "amount=" + amount + '&'
-                + "merchantId=" + merchantId + '&' + "employeeId=" + employeeId + '&'
-                + "bankCode=" + bankCode + '&' + "method=" + method;
-
-            var email = document.getElementById('Email');
-            if (email) {
-                link += '&email=' + email.value
-            }
-            var phoneNo = document.getElementById('Phone Number');
-            if (phoneNo) {
-                link += '&phoneNo=' + phoneNo.value
-            }
-            var orderNo = document.getElementById('Order Number');
-            if (orderNo) {
-                link += '&orderNo=' + orderNo.value
-            }
-            var mandateNo = document.getElementById('Mandate Number');
-            if (mandateNo) {
-                link += '&mandateNo=' + mandateNo.value
-            }
-            var businessModel = document.getElementById('Business Model');
-            if (businessModel) {
-                link += '&businessModel=' + businessModel.value
-            }
-            var notes = document.getElementById('Notes');
-            if (notes) {
-                link += '&notes=' + notes.value
-            }
-            var merchantUrl = document.getElementById('Merchant URL');
-            if (merchantUrl) {
-                link += '&merchantUrl=' + merchantUrl.value
-            }
-            var checksum = document.getElementById('Checksum');
-            if (checksum) {
-                link += '&checksum=' + checksum.value
-            }
-            var details = document.getElementById('Details');
-            if (details) {
-                link += '&details=' + details.value
-            }
-
-            window.location.href = link;
+        if (form === "Instant") {
+            document.getElementById('submitInstant').style.display = "block";
+            document.getElementById('submitMandate').style.display = "none";
+            formType = form;
+        } else {
+            document.getElementById('submitMandate').style.display = "block";
+            document.getElementById('submitInstant').style.display = "none";
+            formType = form;
         }
 
 
     }
 
+    handleSubmitInstant = (e) => {
+        e.preventDefault();
+        var errMsg = "Please add ";
+        var mandatoryInstantFields = ['Amount', 'Bank List'];
+        var missingFields = false;
+        mandatoryInstantFields.map(field => {
+            if (!document.getElementById(field)) {
+                missingFields = true;
+                errMsg += field + ' , ';
+            }
+        })
+        var validateInstant = true;
+        // Check each field
+        this.state.taskIds.map(taskId => {
+            if (this.state.tasks[taskId]['inputField'] === 'TextField' || this.state.tasks[taskId]['inputField'] === 'BankList') {
+                if (document.getElementById(this.state.tasks[taskId]['content']).value !== "") {
+                    if (document.getElementById('alert' + this.state.tasks[taskId]['content']).innerHTML !== "") {
+                        validateInstant = false;
+                    }
+                } else {
+                    document.getElementById('alert' + this.state.tasks[taskId]['content']).innerHTML = 'Invalid ' + this.state.tasks[taskId]['content'] + '.';
+                    validateInstant = false;
+                }
+            }
+        })
+        // Generate URL
+        if (missingFields === false) {
+            if (validateInstant === true) {
+
+                var url = "https://demo.curlec.com/new-instant-pay?";
+                var description = "Instant%20Pay%20Testing";
+                var amount = document.getElementById('Amount').value;
+                var merchantId = "1";
+                var employeeId = "2";
+                var bankCode = document.getElementById('Bank List').value;
+                var method = "03";
+                link = url + "description=" + description + "&amount=" + amount
+                    + "&merchantId=" + merchantId + "&employeeId=" + employeeId
+                    + "&bankCode=" + bankCode + "&method=" + method;
+
+                // Optional fields
+                var email = document.getElementById('Email');
+                if (email) {
+                    link += '&email=' + email.value
+                }
+                var phoneNo = document.getElementById('Phone Number');
+                if (phoneNo) {
+                    link += '&phoneNo=' + phoneNo.value
+                }
+                var orderNo = document.getElementById('Order Number');
+                if (orderNo) {
+                    link += '&orderNo=' + orderNo.value
+                }
+                var mandateNo = document.getElementById('Mandate Number');
+                if (mandateNo) {
+                    link += '&mandateNo=' + mandateNo.value
+                }
+                var businessModel = document.getElementById('Business Model');
+                if (businessModel) {
+                    link += '&businessModel=' + businessModel.value
+                }
+                var notes = document.getElementById('Notes');
+                if (notes) {
+                    link += '&notes=' + notes.value
+                }
+                var merchantUrl = document.getElementById('Merchant URL');
+                if (merchantUrl) {
+                    link += '&merchantUrl=' + merchantUrl.value
+                }
+                var checksum = document.getElementById('Checksum');
+                if (checksum) {
+                    link += '&checksum=' + checksum.value
+                }
+                var details = document.getElementById('Details');
+                if (details) {
+                    link += '&details=' + details.value
+                }
+                window.location.href = link;
+            }
+        } else {
+            document.getElementById('alertMissing').innerHTML = errMsg.slice(0, -2);
+        }
+    }
+
+    handleSubmitMandate = (e) => {
+        e.preventDefault();
+        var validateMandate = true;
+        var missingFields = false;
+        var errMsg = "Please add ";
+        var mandatoryInstantFields = ['Amount', 'Bank List', 'Frequency', 'Maximum Frequency',
+            'Purpose Of Payment', 'Business Model', 'Name', 'Email', 'Id Type'];
+        mandatoryInstantFields.map(field => {
+            if (document.getElementById(field) === null) {
+                missingFields = true;
+                errMsg += field + " , ";
+            }
+        })
+        // Check each field
+        this.state.taskIds.map(taskId => {
+            if (this.state.tasks[taskId]['inputField'] === 'TextField') {
+                if (document.getElementById(this.state.tasks[taskId]['content']).value !== "") {
+                    if (document.getElementById('alert' + this.state.tasks[taskId]['content']).innerHTML !== "") {
+                        validateMandate = false;
+                    }
+                } else {
+                    document.getElementById('alert' + this.state.tasks[taskId]['content']).innerHTML = 'Invalid ' + this.state.tasks[taskId]['content'] + '.';
+                    validateMandate = false;
+                }
+            }
+        })
+
+        if (missingFields === false) {
+            if (validateMandate === true) {
+                var url = "https://demo.curlec.com/new-mandate?";
+                var frequency = document.getElementById("Frequency").value;
+                var maximumFrequency = document.getElementById("Maximum Frequency").value;
+                var purposeOfPayment = document.getElementById("Purpose Of Payment").value;
+                var amount = document.getElementById('Amount').value;
+                var businessModel = document.getElementById('Business Model').value;
+                var name = document.getElementById('Name').value;
+                var emailAddress = document.getElementById('Email').value;
+                var idType = document.getElementById('Id Type').value;
+                var idValue = 88585858599;
+                var bankId = document.getElementById('Bank List').value;
+                var method = "00";
+                var merchantId = "1";
+                var employeeId = "2";
+                var effectiveDate = "2020-07-15";
+
+                link = url + "frequency=" + frequency + "&amount=" + amount
+                    + "&maximumFrequency=" + maximumFrequency + "&purposeOfPayment=" + purposeOfPayment
+                    + "&businessModel=" + businessModel + "&name=" + name + "&method=" + method
+                    + "&emailAddress=" + emailAddress + "&idType=" + idType
+                    + "&idValue=" + idValue + "&bankId=" + bankId
+                    + "&merchantId=" + merchantId + "&employeeId=" + employeeId + "&effectiveDate=" + effectiveDate;
+
+                window.location.href = link;
+            }
+        } else {
+            document.getElementById('alertMissing').innerHTML = errMsg.slice(0, -2);
+
+        }
+    }
 
     render() {
 
         return (
             <div>
-                <button onClick={this.handleGetData}>Get Data</button>
+                <button onClick={() => this.handleGetData("Instant")}>Render Instant Pay Form</button>
+                <button onClick={() => this.handleGetData("Mandate")}>Render Mandate Form</button>
                 <h1>{Object.keys(this.state.newForm)}</h1>
-                <form name="signUp" onSubmit={this.handleSubmit}>
+                <form name="signUp">
                     {this.state.taskIds.map(taskId => (
                         <Renderer key={this.state.tasks[taskId]['content']} id={this.state.tasks[taskId]['content']}
-                            taskId={taskId} Regex={this.state.Regex} task={this.state.tasks[taskId]} bankList={this.state.bankList}></Renderer>
+                            taskId={taskId} Regex={this.state.Regex} task={this.state.tasks[taskId]} bankList={this.state.bankList}
+                            formType={formType}></Renderer>
                     ))}
                     <br />
-                    <button id="submit" onClick={this.handleSubmit}>Submit</button>
+                    <span id="alertMissing" className="errorSpan"></span>
+                    <button id="submitInstant" onClick={this.handleSubmitInstant}>Submit</button>
+                    <button id="submitMandate" onClick={this.handleSubmitMandate}>Submit</button>
                 </form>
-            </div>
+            </div >
         );
     }
 }
 
 
 export default MainPage;
-//https://demo.curlec.com/new-instant-pay?description=Instant%20Pay%20Testing&amount=123&merchantId=1&employeeId=2&bankCode=TEST0021&method=03&email=nguleychun@gmail.com&phoneNo=01116687812
--- src/components/Renderer.jsx
+++ src/components/Renderer.jsx
@@ -3,9 +3,8 @@ import '../style/style.css';
 
 
 
-export default function Renderer({ task, Regex, taskId, bankList }) {
+export default function Renderer({ task, Regex, taskId, bankList, formType }) {
     const loadBanks = (Id) => {
-
         var method = "00";
         var msgToken = "01";
         var xmlhttp = new XMLHttpRequest();
@@ -23,7 +22,12 @@ export default function Renderer({ task, Regex, taskId, bankList }) {
                         for (var i = 0, l = options.length; i < l; i++) {
                             var option = options[i];
                             if (selectBox !== null) {
-                                selectBox.options.add(new Option(option.display_name, option.code));
+                                if (formType === "Instant") {
+                                    selectBox.options.add(new Option(option.display_name, option.code));
+                                } else {
+                                    selectBox.options.add(new Option(option.display_name, option.id));
+                                }
+
                             }
                         }
                     }
@@ -72,6 +76,7 @@ export default function Renderer({ task, Regex, taskId, bankList }) {
                     placeholder={`Please enter your ${task.content.toLowerCase()}`} />
                 <br />
                 <span id={combineId(task.content)} className="errorSpan"></span>
+                <br />
             </div>
         )
     } else if (task.inputField === 'Checkbox') {
@@ -110,6 +115,63 @@ export default function Renderer({ task, Regex, taskId, bankList }) {
                 <span id={combineId(task.content)} className="errorSpan"></span>
                 <br />
                 <script type="text/javascript">{document.onload = loadBanks(task.content)}</script>
+            </div>
+
+        )
+    } else if (task.inputField === 'Frequency') {
+        return (
+            <div>
+                <label htmlFor={task.content}>{task.content}</label>
+                <br />
+                <select id={task.content} name={task.content} required>
+                    <option value="YEARLY">YEARLY</option>
+                    <option value="MONTHLY" defaultValue>MONTHLY</option>
+                    <option value="WEEKLY">WEEKLY</option>
+                    <option value="DAILY">DAILY</option>
+                </select>
+                <br />
+                <br />
+            </div>
+
+        )
+    } else if (task.inputField === 'BusinessModel') {
+        return (
+            <div>
+                <label htmlFor={task.content}>{task.content}</label>
+                <br />
+                <select id={task.content} name={task.content} required>
+                    <option value="B2C" defaultValue>B2C</option>
+                    <option value="B2B1">B2B1</option>
+                </select>
+                <br />
+                <br />
+            </div>
+
+        )
+    } else if (task.inputField === 'Date') {
+        return (
+            <div>
+                <label htmlFor={task.content}>{task.content}</label>
+                <br />
+                <input type="date" id={task.content} />
+                <br />
+                <br />
+            </div>
+
+        )
+    } else if (task.inputField === 'idType') {
+        return (
+            <div>
+                <label htmlFor={task.content}>{task.content}</label>
+                <br />
+                <select id={task.content} name={task.content} required>
+                    <option value="PASSPORT_NUMBER" selected>Passport Number</option>
+                    <option value="NRIC">NRIC</option>
+                    <option value="BUSINESS_REGISTRATION_NUMBER">Business Registration Number</option>
+                    <option value="OTHERS">Others</option>
+                </select>
+                <br />
+                <br />
             </div>
 
         )
